b73638b7cb21a52323209a2164d9feab954adb2e
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2024-12-30 00:03:01 +0700

    Membuat DestinasiHome dengan val route,titleRes

---
+++ app/src/main/java/com/example/pertemuan11/ui/view/mahasiswa/HomeView.kt
@@ -0,0 +1,9 @@
+package com.example.pertemuan11.ui.view.mahasiswa
+
+
+import com.example.pertemuan11.ui.navigation.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Home Mhs"
+}
