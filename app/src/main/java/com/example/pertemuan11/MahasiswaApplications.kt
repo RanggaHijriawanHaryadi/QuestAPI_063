dbfe2d12b51f60f7e040d64776d6bb0cc390c2b9
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2024-12-30 00:39:21 +0700

    Memodifikasi nama class menjadi MahasiswaApplications

--- app/src/main/java/com/example/pertemuan11/MahasiswaApplications.kt
+++ app/src/main/java/com/example/pertemuan11/MahasiswaApplications.kt
@@ -4,7 +4,7 @@ import android.app.Application
 import com.example.pertemuan11.dependeciesinjection.AppContainer
 import com.example.pertemuan11.dependeciesinjection.MahasiswaContainer
 
-class MahasiswaApplication:Application() {
+class MahasiswaApplications:Application() {
     lateinit var container: AppContainer
     override fun onCreate() {
         super.onCreate()
