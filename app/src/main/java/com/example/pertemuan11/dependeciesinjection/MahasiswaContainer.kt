87d578b098b624cf6c7900d36aa7758a1e61da31
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2024-12-29 15:16:59 +0700

    Membuat MahasiswaContainer dengan url menghubungkan umyTI

---
+++ app/src/main/java/com/example/pertemuan11/dependeciesinjection/MahasiswaContainer.kt
@@ -0,0 +1,27 @@
+package com.example.pertemuan11.dependeciesinjection
+
+import com.example.pertemuan11.repository.MahasiswaRepository
+import com.example.pertemuan11.repository.NetworkMahasiswaRepository
+import com.example.pertemuan11.servis.MahasiswaServis
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+interface AppContainer{
+    val kontakRepository: MahasiswaRepository
+}
+class MahasiswaContainer: AppContainer {
+    private val baseUrl= "http://10.0.2.2:8000/umyTI/" //localhost diganti ip klo run dihp
+    private val json = Json { ignoreUnknownKeys = true }
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl).build()
+
+    private val mahasiswaServis: MahasiswaServis by lazy {
+        retrofit.create(MahasiswaServis::class.java)
+    }
+    override val kontakRepository: MahasiswaRepository by lazy {
+        NetworkMahasiswaRepository(mahasiswaServis)
+    }
+}
