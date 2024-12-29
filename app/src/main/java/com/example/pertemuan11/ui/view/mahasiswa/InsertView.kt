110342bf034578fb3b618c43d7fb7b2f9fc38efe
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2024-12-29 23:43:02 +0700

    Membuat DestinasiEntry dengan val route, titleRes

---
+++ app/src/main/java/com/example/pertemuan11/ui/view/mahasiswa/InsertView.kt
@@ -0,0 +1,8 @@
+package com.example.pertemuan11.ui.view.mahasiswa
+
+import com.example.pertemuan11.ui.navigation.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi{
+    override val route= "item_entry"
+    override val titleRes = "Entry Mhs"
+}
