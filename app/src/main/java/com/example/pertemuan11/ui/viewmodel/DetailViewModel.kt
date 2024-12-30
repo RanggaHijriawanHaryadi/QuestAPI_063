a2d2fec5a598dccfbb2a97f51ad11176a7c8ab59
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2024-12-30 12:20:19 +0700

    Menambahkan get data mahasiswabyNim, deleteMhs pada DetailViewModel

--- app/src/main/java/com/example/pertemuan11/ui/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/pertemuan11/ui/viewmodel/DetailViewModel.kt
@@ -1,11 +1,66 @@
 package com.example.pertemuan11.ui.viewmodel
 
-
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pertemuan11.model.Mahasiswa
+import com.example.pertemuan11.repository.MahasiswaRepository
+import com.example.pertemuan11.ui.view.mahasiswa.DestinasiDetail
+import kotlinx.coroutines.launch
 
 
+class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val mahasiswaRepository: MahasiswaRepository
+) : ViewModel() {
+    private val nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
 
+    var detailUiState: DetailUiState by mutableStateOf(DetailUiState())
+        private set
 
+    init {
+        getMahasiswaByNim()
+    }
+
+    private fun getMahasiswaByNim() {
+        viewModelScope.launch {
+            detailUiState = DetailUiState(isLoading = true)
+            try {
+                val result = mahasiswaRepository.getMahasiswaByNim(nim)
+                detailUiState = DetailUiState(
+                    detailUiEvent = result.toDetailUiEvent(),
+                    isLoading = false
+                )
+            } catch (e: Exception) {
+                detailUiState = DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = e.message ?: "Unknown"
+                )
+            }
+        }
+    }
+
+    fun deleteMhs() {
+        viewModelScope.launch {
+            detailUiState = DetailUiState(isLoading = true)
+            try {
+                mahasiswaRepository.deleteMahasiswa(nim)
+
+                detailUiState = DetailUiState(isLoading = false)
+            } catch (e: Exception) {
+                detailUiState = DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = e.message ?: "Unknown Error"
+                )
+            }
+        }
+    }
+}
 
 
 data class DetailUiState(
